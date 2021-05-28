f3fea3760a54089d7f86b447d1b1f8384277e99e
Jared LaSante <jaredlasante@gmail.com>
2021-05-28 11:14:37 -0400

    Updated version and added SPM support

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "SwiftOverlays",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(name: "SwiftOverlays", targets: ["SwiftOverlays"])
+    ],
+    targets: [
+        .target(
+            name: "SwiftOverlays",
+            path: "Source"
+        )
+     ]
+)
